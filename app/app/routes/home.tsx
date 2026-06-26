afd52d2cbff9991d600d020c353d377c86f8276a
D-Vitale <davidee.vitalee@gmail.com>
2026-06-26 11:04:39 +0200

    AUGURI ERRO!

--- app/app/routes/home.tsx
+++ app/app/routes/home.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return null;
+	return (
+		<div className={"flex items-center justify-center h-screen flex-col"}>
+			<h1 className={"text-5xl font-extrabold text-transparent bg-clip-text bg-linear-to-tl from-blue-600 to-red-500"}>AUGURI ERRO!</h1>
+			<img src="https://miro.medium.com/v2/resize:fit:960/0*BxwKYXDC_iv1vjlo" alt="Auguri ERRO" className="mx-auto my-8" />
+		</div>
+	);
 }
